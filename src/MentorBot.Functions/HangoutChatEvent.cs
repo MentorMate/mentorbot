59bf0d43d850f0206607074e8ee4f6b20a34ce0f
Rosen Kolev <rosen.kolev@mentormate.com>
2018-10-29 18:06:01 +0200

    HOTFIX: The costmo DB attr was not working with latest version.

--- src/MentorBot.Functions/HangoutChatEvent.cs
+++ src/MentorBot.Functions/HangoutChatEvent.cs
@@ -1,18 +1,15 @@
 // Copyright (c) 2018. Licensed under the MIT License. See https://www.opensource.org/licenses/mit-license.php for full license information.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 using MentorBot.Functions.Abstract.Services;
-using MentorBot.Functions.Models.DataResultModels;
+using MentorBot.Functions.App;
 using MentorBot.Functions.Models.Domains;
 using MentorBot.Functions.Models.HangoutsChat;
 using MentorBot.Functions.Models.Options;
-using MentorBot.Localize;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,8 +19,6 @@ using Microsoft.Extensions.Logging;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-
-using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
 
 namespace MentorBot.Functions
 {
@@ -41,41 +36,23 @@ namespace MentorBot.Functions
         [FunctionName("chatEvent")]
         public static async Task<IActionResult> RunAsync(
             [HttpTrigger(AuthorizationLevel.Anonymous, nameof(HttpMethods.Post), Route = null)] HttpRequestMessage req,
-            [CosmosDB("mentorbot", "messages", CreateIfNotExists = true, ConnectionStringSetting = "CosmosDBConnection")] IAsyncCollector<Message> messages,
-            [Inject] IHangoutsChatService hangoutsChatService,
-            [Inject] GoogleCloudOptions options,
-            [Inject] IStringLocalizer localizer,
             ILogger log)
         {
-            if (req == null)
-            {
-                return new ObjectResult("Request not initialized")
-                {
-                    StatusCode = (int)HttpStatusCode.InternalServerError
-                };
-            }
+            Debug.Write(req);
+            ServiceLocator.EnsureServiceProvider();
 
-            if (hangoutsChatService == null ||
-                localizer == null ||
-                log == null ||
-                options == null)
-            {
-                return new ObjectResult("Services not initialized")
-                {
-                    StatusCode = (int)HttpStatusCode.InternalServerError
-                };
-            }
+            var content = req.Content ?? throw new ArgumentNullException(nameof(req));
+            var client = ServiceLocator.Get<IDocumentClientService>();
+            var hangoutsChatService = ServiceLocator.Get<IHangoutsChatService>();
+            var options = ServiceLocator.Get<GoogleCloudOptions>();
 
-            log.LogInformation(localizer["C# HTTP trigger function processed a request."]);
-
-            var hangoutChatEvent = await req
-                .Content
+            var hangoutChatEvent = await content
                 .ReadAsAsync<ChatEvent>()
                 .ConfigureAwait(true);
 
             if (!hangoutChatEvent.Token.Equals(options.HangoutChatRequestToken, StringComparison.InvariantCulture))
             {
-                log.LogError(localizer["The tokens do not match. Unauthorized access."]);
+                log.LogError("The tokens do not match. Unauthorized access. " + hangoutChatEvent.Token);
                 return new UnauthorizedResult();
             }
 
@@ -83,43 +60,17 @@ namespace MentorBot.Functions
                 .BasicAsync(hangoutChatEvent)
                 .ConfigureAwait(false);
 
-            if (messages != null)
-            {
-                await messages.AddAsync(result).ConfigureAwait(false);
-            }
+            var document = await client
+                .GetAsync<Message>("mentorbot", "messages")
+                .ConfigureAwait(false);
+
+            await document
+                .AddAsync(result)
+                .ConfigureAwait(false);
 
             log.LogInformation(result.Output?.Text);
 
             return new JsonResult(result.Output, JsonSettings);
-        }
-
-        /// <summary>The main Azure function.</summary>
-        [FunctionName("get-messages-stats")]
-        public static IActionResult GetMessagesStatistics(
-            [HttpTrigger(AuthorizationLevel.Anonymous, nameof(HttpMethods.Get), Route = null)] HttpRequest req,
-            [CosmosDB("mentorbot", "messages", ConnectionStringSetting = "CosmosDBConnection", SqlQuery = "SELECT TOP 1000 m.ProbabilityPercentage FROM messages m")] IEnumerable<Message> messages,
-            [Inject] IStringLocalizer localizer,
-            ILogger log)
-        {
-            if (localizer == null || req == null)
-            {
-                return new ObjectResult("Request not initialized")
-                {
-                    StatusCode = (int)HttpStatusCode.InternalServerError
-                };
-            }
-
-            log.LogInformation(localizer["C# HTTP trigger function get-messages-stats."]);
-
-            var result = messages
-                .GroupBy(it => it.ProbabilityPercentage / 10)
-                .Select(group => new MessagesStatistic
-                {
-                    ProbabilityPercentage = (byte)(group.Key * 10),
-                    Count = group.Count()
-                });
-
-            return new JsonResult(result, JsonSettings);
         }
     }
 }
---
+++ src/MentorBot.Functions/Services/DocumentClientService.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) 2018. Licensed under the MIT License. See https://www.opensource.org/licenses/mit-license.php for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MentorBot.Functions.Abstract.Services;
+
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace MentorBot.Functions.Services
+{
+    /// <summary>An document database service for Azure Comtoso DB.</summary>
+    public sealed class DocumentClientService : IDocumentClientService
+    {
+        private static readonly ConnectionPolicy Policy = new ConnectionPolicy
+        {
+            ConnectionMode = ConnectionMode.Direct,
+            ConnectionProtocol = Protocol.Tcp,
+            RetryOptions = new RetryOptions
+            {
+                MaxRetryAttemptsOnThrottledRequests = 10,
+                MaxRetryWaitTimeInSeconds = 30
+            }
+        };
+
+        private readonly string _accountEndpoint;
+        private readonly string _authKeyOrResourceToken;
+        private readonly Lazy<IDocumentClient> _client;
+
+        /// <summary>Initializes a new instance of the <see cref="DocumentClientService"/> class.</summary>
+        public DocumentClientService(string accountEndpoint, string authKeyOrResourceToken)
+        {
+            _accountEndpoint = accountEndpoint;
+            _authKeyOrResourceToken = authKeyOrResourceToken;
+            _client = new Lazy<IDocumentClient>(() => new DocumentClient(new Uri(_accountEndpoint), _authKeyOrResourceToken, Policy));
+        }
+
+        /// <inheritdoc/>
+        public async Task<IDocument<T>> GetAsync<T>(string databaseName, string collectionName)
+        {
+            var client = _client.Value;
+            var db = new Database { Id = databaseName };
+            var documentCollection = new DocumentCollection { Id = collectionName };
+
+            var databaseLink = UriFactory.CreateDatabaseUri(databaseName);
+            var documentCollectionLink = UriFactory.CreateDocumentCollectionUri(databaseName, collectionName);
+
+            await client.CreateDatabaseIfNotExistsAsync(db)
+                        .ConfigureAwait(false);
+
+            await client.CreateDocumentCollectionIfNotExistsAsync(databaseLink, documentCollection)
+                        .ConfigureAwait(false);
+
+            return new Document<T>(client, documentCollectionLink);
+        }
+
+        /// <summary>A document collection resource.</summary>
+        /// <typeparam name="T">The document reource type.</typeparam>
+        public class Document<T> : IDocument<T>
+        {
+            private readonly IDocumentClient _client;
+            private readonly Uri _documentCollectionUri;
+
+            /// <summary>Initializes a new instance of the <see cref="Document{T}"/> class.</summary>
+            public Document(IDocumentClient client, Uri documentCollectionUri)
+            {
+                _client = client;
+                _documentCollectionUri = documentCollectionUri;
+            }
+
+            /// <inheritdoc/>
+            public Task AddAsync(T item) =>
+                _client.CreateDocumentAsync(_documentCollectionUri, item);
+
+            /// <inheritdoc/>
+            public IReadOnlyList<T> Query(string sqlExpression) =>
+                _client.CreateDocumentQuery<T>(_documentCollectionUri, sqlExpression)
+                       .ToList();
+        }
+    }
+}
