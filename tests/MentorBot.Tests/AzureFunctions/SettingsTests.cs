bc3a9c2282b9dfef14ef325656ae2e2de78da2dc
svilen-nedev <svilen.nedev@mentormate.com>
2019-04-10 15:54:49 +0300

    Added unit tests for Settings function and TableStorageService.

---
+++ tests/MentorBot.Tests/AzureFunctions/SettingsTests.cs
@@ -0,0 +1,116 @@
+﻿using MentorBot.Functions.Abstract.Services;
+using MentorBot.Functions.App;
+using MentorBot.Functions.AzureFunctions;
+using MentorBot.Functions.Models.Settings;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentorBot.Tests.AzureFunctions
+{
+    [TestClass]
+    [TestCategory("Functions")]
+    public sealed class SettingsTests
+    {
+        [TestMethod]
+        public async Task GetSettings_HappyPath_GET()
+        {
+            var storageService = Substitute.For<IStorageService>();
+
+            storageService.GetSettingsAsync().Returns(new MentorBotSettings { Processors = new List<ProcessorSettings> {
+                new ProcessorSettings { Name = "Processor 1", Enabled = true },
+                new ProcessorSettings { Name = "Processor 2", Enabled = false },
+            } });
+
+            ServiceLocator.DefaultInstance.BuildServiceProviderWithDescriptors(
+                new ServiceDescriptor(typeof(IStorageService), storageService));
+
+            Microsoft.Extensions.Logging.ILogger logger = Substitute.For<Microsoft.Extensions.Logging.ILogger>();
+
+            HttpContext context = new DefaultHttpContext();
+            context.Request.Method = "GET";
+
+            var result = await Settings.Run(context.Request, logger) ;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            Assert.IsNotNull((result as OkObjectResult).Value);
+            Assert.IsInstanceOfType((result as OkObjectResult).Value, typeof(MentorBotSettings));
+
+            var settings = (result as OkObjectResult).Value as MentorBotSettings;
+            Assert.AreEqual("MentorBotSettings", settings.Key);
+            Assert.IsNotNull(settings.Processors);
+            Assert.AreEqual("Processor 1", settings.Processors[0].Name);
+            Assert.IsTrue(settings.Processors[0].Enabled);
+            Assert.AreEqual("Processor 2", settings.Processors[1].Name);
+            Assert.IsFalse(settings.Processors[1].Enabled);
+        }
+
+        [TestMethod]
+        public async Task GetSettings_HappyPath_POST()
+        {
+            var storageService = Substitute.For<IStorageService>();
+            var settingsToSave = new MentorBotSettings
+            {
+                Processors = new List<ProcessorSettings>
+                {
+                    new ProcessorSettings { Name = "Processor 1", Enabled = true },
+                    new ProcessorSettings { Name = "Processor 2", Enabled = false },
+                }
+            };
+            storageService.SaveSettingsAsync(Arg.Is<MentorBotSettings>(s => 
+                s.Processors != null && 
+                s.Processors.Count == 2 &&
+                s.Processors[0].Name == "Processor 1" && s.Processors[0].Enabled == true &&
+                s.Processors[1].Name == "Processor 2" && s.Processors[1].Enabled == false)).Returns(true);
+
+            ServiceLocator.DefaultInstance.BuildServiceProviderWithDescriptors(
+                new ServiceDescriptor(typeof(IStorageService), storageService));
+
+            Microsoft.Extensions.Logging.ILogger logger = Substitute.For<Microsoft.Extensions.Logging.ILogger>();
+
+            HttpContext context = new DefaultHttpContext();
+            context.Request.Method = "POST";
+            context.Request.Body = new MemoryStream(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(settingsToSave)));
+
+            var result = await Settings.Run(context.Request, logger) ;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            Assert.IsNotNull((result as OkObjectResult).Value);
+            Assert.IsInstanceOfType((result as OkObjectResult).Value, typeof(MentorBotSettings));
+
+            var settings = (result as OkObjectResult).Value as MentorBotSettings;
+            Assert.AreEqual("MentorBotSettings", settings.Key);
+            Assert.IsNotNull(settings.Processors);
+            Assert.AreEqual("Processor 1", settings.Processors[0].Name);
+            Assert.IsTrue(settings.Processors[0].Enabled);
+            Assert.AreEqual("Processor 2", settings.Processors[1].Name);
+            Assert.IsFalse(settings.Processors[1].Enabled);
+        }
+
+        [TestMethod]
+        public async Task GetSettings_BadRequest()
+        {
+            Microsoft.Extensions.Logging.ILogger logger = Substitute.For<Microsoft.Extensions.Logging.ILogger>();
+
+            HttpContext context = new DefaultHttpContext();
+            context.Request.Method = "PUT";
+
+            var result = await Settings.Run(context.Request, logger) ;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+            Assert.AreEqual("Unsupported method", (result as BadRequestObjectResult).Value);
+        }
+    }
+}
---
+++ tests/MentorBot.Tests/Business/Services/TableStorageServiceTests.cs
@@ -0,0 +1,123 @@
+﻿using CoreHelpers.WindowsAzure.Storage.Table;
+using MentorBot.Functions.Models.Domains;
+using MentorBot.Functions.Models.Settings;
+using MentorBot.Functions.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentorBot.Tests.Business.Services
+{
+    [TestClass]
+    [TestCategory("Business.Services")]
+    public sealed class TableStorageServiceTests
+    {
+        private TableStorageService _serviceConnected;
+        private TableStorageService _serviceNotConnected;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _serviceConnected = new TableStorageService(new Functions.Models.Options.AzureCloudOptions("connection string", string.Empty, string.Empty, string.Empty));
+            _serviceNotConnected = new TableStorageService(null);
+        }
+
+        #region NotConnected tests
+
+        [TestMethod]
+        public async Task AddUsersAsync_NotConnected_returns_False()
+        {
+            var result = await _serviceNotConnected.AddUsersAsync(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task UpdateUsersAsync_NotConnected_returns_False()
+        {
+            var result = await _serviceNotConnected.UpdateUsersAsync(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task GetAllUsersAsync_NotConnected_returns_EmptyList()
+        {
+            var result = await _serviceNotConnected.GetAllUsersAsync();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IReadOnlyList<User>));
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetUsersByIdListAsync_NotConnected_returns_EmptyList()
+        {
+            var result = await _serviceNotConnected.GetUsersByIdListAsync(null);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IReadOnlyList<User>));
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public async Task AddAddressesAsync_NotConnected_returns_False()
+        {
+            var result = await _serviceNotConnected.AddAddressesAsync(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task GetAddressesAsync_NotConnected_returns_EmptyList()
+        {
+            var result = await _serviceNotConnected.GetAddressesAsync();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IReadOnlyList<GoogleAddress>));
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetMessagesAsync_NotConnected_returns_EmptyList()
+        {
+            var result = await _serviceNotConnected.GetMessagesAsync();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IReadOnlyList<Message>));
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public async Task SaveMessageAsync_NotConnected_returns_False()
+        {
+            var result = await _serviceNotConnected.SaveMessageAsync(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task GetSettingsAsync_NotConnected_returns_EmptySettings()
+        {
+            var result = await _serviceNotConnected.GetSettingsAsync();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(MentorBotSettings));
+            Assert.AreEqual(nameof(MentorBotSettings), result.Key);
+            Assert.AreEqual(0, result.Processors.Count);
+        }
+
+        [TestMethod]
+        public async Task SaveSettingsAsync_NotConnected_returns_False()
+        {
+            var result = await _serviceNotConnected.SaveSettingsAsync(null);
+
+            Assert.IsFalse(result);
+        }
+
+        #endregion
+    }
+}
