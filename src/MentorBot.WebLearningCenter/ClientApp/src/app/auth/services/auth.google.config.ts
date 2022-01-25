72a09c5dcb84d9c561887fc659f727af1afa1bdb
Rosen Kolev <rosen.kolev@hotmail.com>
2022-01-25 22:41:33 +0200

    Fix build

--- src/MentorBot.WebLearningCenter/ClientApp/src/app/auth/services/auth.google.config.ts
+++ src/MentorBot.WebLearningCenter/ClientApp/src/app/auth/services/auth.google.config.ts
@@ -4,7 +4,7 @@ import { environment } from '../../../environments/environment';
 export const authConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
   redirectUri: environment.webPath + 'app/signin-google',
-  clientId: environment.googleClientId,
+  clientId: environment.googleClientId as unknown as string,
   scope: 'openid profile email',
   sessionChecksEnabled: true,
   strictDiscoveryDocumentValidation: false,
